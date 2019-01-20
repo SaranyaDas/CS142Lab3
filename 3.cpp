ef4f84f588a8c162879459ea533496b7ebff1aa4
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2019-01-20 23:24:34 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,168 +1,137 @@
 #include <iostream>
 using namespace std;
-class node
-{
+class Node {
 public:
-	//pointer to the previous node
-	node*pred;
-	//pointer to the next node
-	node*succ;
-	//data
-	int data;
-	//default constructor
-	node()
-	{
-		pred=NULL;
-		succ=NULL;
+	//Pointer to previous node
+	Node * prev;
+	//Pointer to next node
+	Node * next;
+	int value;
+	node() {
+		prev = NULL;
+		next = NULL;
 	}
-};
-class Dlist
-{
+}
+class DLList {
 public:
-	//the pointer to first node
-	node*head;
-	//pointer to the last node
-	node*tail;
-	//Default constructor
-	Dlist()
-	{
-		head=NULL;
-		tail=NULL;
+	//Pointer to first node
+	Node * head;
+	//Pointer to last node
+	Node * tail;
+	DLList() {
+		head = NULL;
+		tail = NULL;
 	}
-	void insert(int value)
-	{
-		node*temp=new node;
-		temp->data=value;
-		if (head==NULL) head=temp;
-		else
-		{
-			tail->succ=temp;
-			temp->pred=tail;
+	void insert(int data) {
+		Node * temp = new Node;
+		temp -> value = data;
+		if (head == NULL) head = temp;
+		else {
+			tail -> next = temp;
+			temp -> prev = tail;
 		}
-		tail=temp;
+		tail = temp;
 	}
-	void insertAt(int position,int value)
-	{
-		node*t=head;
-		int j=0;
-		while (t!=NULL)
-		{
-			t=t->succ;
+	void insertAt(int pos, int data) {
+		Node * i = head;
+		int j = 0;
+		while (i != NULL) {
+			i = i -> next;
 			j++;
 		}
-		cout<<j<<endl;
-		node*current=head;
-		int i=1;
-		if(j>=position)
-		{	
-			while (i<position-1)
-			{
-				i++;
-				current=current->succ;
+		cout << j << endl;
+		Node * current = head;
+		int k = 1;
+		if (j >= pos) {	
+			while (k < pos - 1) {
+				k++;
+				current = current -> next;
 			}
-			//cout<<current->data<<endl;
-			//create a free node
-			node*temp=new node;
-			//assign it the required value
-			temp->data=value;
-			//make temp's successor current's successor
-			temp->succ=current->succ;
-			//make temp's predecessor as the current node
-			temp->pred=current;
-			//make current's successor's predecessor as temp
-			current->succ->pred=temp;
-			//make current's successor as temp
-			current->succ=temp;
-			//note that the order is really important 
+			cout << current -> value << endl;
+			Node * temp = new Node;
+			temp -> value = data;
+			temp -> next = current -> next;
+		        temp -> prev = current;
+			current -> next -> prev = temp;
+			current -> next = temp;
 		}
-		else
-		{
-			cout<<"Check the number of elements in the list."<<endl;
+		else {
+			cout << "Check the number of elements in the list." << endl;
 		}
 	}
-	void del()
-	{
-		node*t=tail;
-		node*current=head;
-		while(current->succ->succ!=NULL)
-		{
-			current=current->succ;
+	void delete() {
+		Node * i = tail;
+		Node * current = head;
+		while(current -> next -> next != NULL) {
+			current = current -> next;
 		}
-		cout<<current->data<<endl;
-		current->succ=NULL;
-		current=tail;
-		delete t;
+		cout << current -> value << endl;
+		current -> next = NULL;
+		current = tail;
+		delete i;
 	}
-	void delAt(int position)
-	{
-		node*q=head;
-		int j=0;
-		while (q!=NULL)
-		{
-			q=q->succ;
+	void deleteAt(int pos)	{
+		Node * l = head;
+		int j = 0;
+		while (l != NULL) {
+			l = l -> next;
 			j++;
 		}
-		if (j>=position)
-		{
-			node*current=head;
-			int i=1;
-			while (i<position-1)
-			{
-				current=current->succ;
+		if (j >= pos) {
+			Node * current = head;
+			int k = 1;
+			while (k < pos - 1) {
+				current = current -> next;
 			}
-			node*temp=current->succ;
-			current->succ=temp->succ;
-			temp->succ->pred=current;
+			Node * temp = current -> next;
+			current -> next = temp -> next;
+			temp -> next -> prev = current;
 			delete temp;
 		}
-		else cout<<"Check the number of entries in the list."<<endl; 
+		else {
+			cout << "Check the number of entries in the list." << endl; 
 	}
-	void count()
-	{
-		node*t=head;
-		int i=0;
-		while (t!=NULL)
-		{
-			t=t->succ;
-			i++;
+	void countItems() {
+		Node * k = head;
+		int k = 0;
+		while (i != NULL) {
+			i = i -> next;
+			k++;
 		}
-		cout<<"The nummber of entries in your list is : "<<i<<endl;
+		cout << "The nummber of entries in the list is : " << k << endl;
 	}
-	void display()
-	{
-		node*temp=head;
-		while(temp!=NULL)
-		{
-			cout<<temp->data<<" -> ";
-			temp=temp->succ;
+	void display() {
+		Node * temp = head;
+		while(temp != NULL) {
+			cout << temp -> value << " -> ";
+			temp = temp -> next;
 		}
-		cout<<endl;
+		cout << endl;
 	}
-};
-int main()
-{
-	Dlist l;
+}
+int main() {
+	DDList l;
 	l.insert(3);
 	l.insert(4);
 	l.insert(45);
-	l.count();
+	l.countItems();
 	l.display();
 	l.insertAt(3,455);
-	l.count();
+	l.countItems();
 	l.display();
-	l.del();
-	l.count();
+	l.delete();
+	l.countItems();
 	l.display();
 	l.insert(245);
 	l.insert(3535);
 	l.insertAt(3,635);
-	l.count();
+	l.countItems();
 	l.display();
-	l.del();
-	l.count();
+	l.delete();
+	l.countItems();
 	l.display();
-	l.delAt(2);
-	l.count();
+	l.deleteAt(2);
+	l.countItems();
 	l.display();
 	return 0;
 }
